5fd5903544df2db3aaa905cc174364d96dd1e421
Aswaniss <aswanisubramanian30@gmail.com>
2025-03-05 20:40:24 +0530

    url

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.home, name='home'),
+    path('',views.home, name='home'),
     path('registration',views.registration , name ='registration'),
     path('verify_otp/<str:username>/',views.verify_otp, name='verify_otp'),
 
